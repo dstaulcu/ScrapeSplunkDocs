2172eed863a832be537d0dddd203b0ac24643bb7
dstaulcu <David_Staulcup@hotmail.com>
2017-01-03 06:59:25 -0500

    added conf 2016 prezo scraper

---
+++ ScrapeConfPresentations.ps1
@@ -0,0 +1,19 @@
+﻿
+if (!$page) {
+    $url = 'http://conf.splunk.com/sessions/2016-sessions.html'
+    $page = Invoke-WebRequest -Uri $url
+}
+
+foreach ($link in $page.links) {
+#    if (($link.innerHTML -eq 'Recording') -or ($link.innerHTML -eq 'Slides')) {
+    if (($link.innerHTML -eq 'Slides')) {
+        $filepath = $link | Select-Object -ExpandProperty href
+        $filename = ([regex]"(recordings|slides)\/(.*)").match($filepath).Groups[2].Value
+        $domain = 'http://conf.splunk.com'
+        $url = "$domain$filepath"
+        write-host ('downloading ' + $filename)
+        $downloadfile = "$env:TEMP\$filename"
+        $client = new-object System.Net.WebClient 
+        $client.DownloadFile($url,$downloadfile) 
+    }
+}
