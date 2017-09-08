a5fcf5af1b40dbeb6e0e31957ef4667d522c3beb
dstaulcu <dstaulcu@users.noreply.github.com>
2017-09-08 16:50:53 -0400

    added support for older versions of Splunk

--- ScrapeSplunkDocs.ps1
+++ ScrapeSplunkDocs.ps1
@@ -1,18 +1,91 @@
-﻿<#
-.Synopsis
-   Scrapes splunk enterprise documentation from website, copies pdf files into categorized folders, then compresses content.
-#>
-
-
-$docrevlevel = '6.1beta'
+﻿$VerbosePreference = "SilentlyContinue"
+$DebugPreference = "SilentlyContinue"
 
 $StartDate=(GET-DATE)
 
-$scriptpath = split-path $SCRIPT:MyInvocation.MyCommand.Path -parent
 
+function Get-SplunkDoc-Versions {
+    # scrape the splunk enterprise document main page
+    $url = 'http://docs.splunk.com/Documentation/Splunk'
+    $page = Invoke-WebRequest -Uri $url
+
+    # get content of the section which contains options for doc versions
+    $versioncontainer = ($page.AllElements | Where-Object {$_.class -match "(version-select-container)"}).innerhtml
+
+    # cleanup option value list so that we have a comma delimmted list of internal/external version names
+    $versioncontainer = $versioncontainer -split "`<option value`=" 
+    $versioncontainer = $versioncontainer -match "/option"
+    $versioncontainer = $versioncontainer -replace "</option>",""
+    $versioncontainer = $versioncontainer -replace "(\s+</select>)",""
+    $versioncontainer = $versioncontainer -replace "`"",""
+    $versioncontainer = $versioncontainer -replace ">",","
+    $versioncontainer = $versioncontainer -replace " selected=",""
+    $versioncontainer = $versioncontainer -replace "(\n)",""
+
+    return $versioncontainer
+}
+
+function Get-SplunkDoc-UserSelection {
+    param($formtitle="SplunkDocs Downloader",$prompt="Please select a version:",$listitems)
+
+    [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+    [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
+
+    $objForm = New-Object System.Windows.Forms.Form 
+    $objForm.Text = $formtitle #"SplunkDocs Downloader"
+    $objForm.Size = New-Object System.Drawing.Size(350,200) 
+    $objForm.StartPosition = "CenterScreen"
+
+    $objForm.KeyPreview = $True
+    $objForm.Add_KeyDown({if ($_.KeyCode -eq "Enter") 
+        {$x=$objListBox.SelectedItem;$objForm.Close()}})
+    $objForm.Add_KeyDown({if ($_.KeyCode -eq "Escape") 
+        {$objForm.Close()}})
+
+    $OKButton = New-Object System.Windows.Forms.Button
+    $OKButton.Location = New-Object System.Drawing.Size(75,120)
+    $OKButton.Size = New-Object System.Drawing.Size(75,23)
+    $OKButton.Text = "OK"
+    $OKButton.Add_Click({$x=$objListBox.SelectedItem;$objForm.Close()})
+    $objForm.Controls.Add($OKButton)
+
+    $CancelButton = New-Object System.Windows.Forms.Button
+    $CancelButton.Location = New-Object System.Drawing.Size(150,120)
+    $CancelButton.Size = New-Object System.Drawing.Size(75,23)
+    $CancelButton.Text = "Cancel"
+    $CancelButton.Add_Click({$objForm.Close()})
+    $objForm.Controls.Add($CancelButton)
+
+    $objLabel = New-Object System.Windows.Forms.Label
+    $objLabel.Location = New-Object System.Drawing.Size(10,20) 
+    $objLabel.Size = New-Object System.Drawing.Size(280,20) 
+    $objLabel.Text = $prompt # "Please select a version:"
+    $objForm.Controls.Add($objLabel) 
+
+    $objListBox = Out-Null
+    $objListBox = New-Object System.Windows.Forms.ListBox 
+    $objListBox.Location = New-Object System.Drawing.Size(10,40) 
+    $objListBox.Size = New-Object System.Drawing.Size(260,20) 
+    $objListBox.Height = 80
+
+    foreach ($item in $listitems) {
+        [void] $objListBox.Items.Add($item)
+    }
+
+    $objForm.Controls.Add($objListBox) 
+
+    $objForm.Topmost = $True
+
+    $objForm.Add_Shown({$objForm.Activate()})
+    [void] $objForm.ShowDialog()
+
+    return $objListBox.SelectedItem
+}
+
+$client = new-object System.Net.WebClient
+$scriptpath = split-path $SCRIPT:MyInvocation.MyCommand.Path -parent
 $catalogfile = $scriptpath + '\' + 'catalog.csv'
 $catalog = Import-Csv -Path $catalogfile
-
 $downloadfolder = $scriptpath + '\' + 'downloads'
 
 # if the download folder exists, remove it
@@ -22,28 +95,40 @@ if ((Test-Path $downloadfolder -PathType Container) -eq $true) {
 # create the download folder anew
 New-Item -Path $scriptpath -Name "downloads" -ItemType "directory" | Out-Null
 
-# scrape the splunk enterprise document site
-$url = 'http://docs.splunk.com/Documentation/Splunk/latest'
-$page = Invoke-WebRequest -Uri $url
+# scrape the default splunk enterprise documents site for versions of document containers
+$VersionContainer = Get-SplunkDoc-Versions
 
-foreach ($link in $page.links) {
-    if ($link.href -like '*/Documentation/Splunk/*') {
-        $extract = ([regex]"Splunk\/(\S+)\/(\S+)\/(\S+)").match($link.href)
+# prompt the user to select which document container they want to download from
+$listitems = $VersionContainer -replace "(.*),",""
+$selecteditem = Get-SplunkDoc-UserSelection -formtitle "SplunkDocs Downloader" -prompt "Select version to download" -listitems $listitems
+if (!($selecteditem)) { write-verbose "User cancelled action; exiting." ; Exit }
+$selecteditem = ($VersionContainer -like "*$selecteditem*").split(",")[0]
+write-host "User selected document container version $($selecteditem)."
 
-        if ($extract.Success -eq $true) { 
+# download the main page of the selected document container
+$containerUrl = "http://docs.splunk.com/Documentation/Splunk/$($selecteditem)"
+Write-Debug "Downloading main page of document container: $($containerUrl)"
+$containerPage = Invoke-WebRequest -Uri $containerUrl
 
-            $version = $extract.Groups[1].Value
-            $class = $extract.Groups[2].Value
-            $doc = $extract.Groups[3].Value
-            $url = 'https://docs.splunk.com/index.php?title=Documentation:Splunk:' + $class + ':' + $doc + ':' + $docrevlevel + '&action=pdfbook&version=' + $version + 'product=Splunk'
+# search version container for instances of document page links
 
-            $filename = ($link.innerText).Trim() + '.pdf'
-            $downloadfile = $downloadfolder + '\' + $filename
-            write-host ('downloading ' + $filename)
+foreach ($link in $containerPage.links) {
+    if ($link.href -like '*/Documentation/Splunk/*/*') {
 
-            $client = new-object System.Net.WebClient 
-            $client.DownloadFile($url,$downloadfile) 
-            }
+        $docname = $link.outerText.trim()
+        $docname = "$($docname).pdf"
+        $downloadfile = $downloadfolder + '\' + $docname
+
+        write-host "Downloading $($docname) for version $($selecteditem)."
+        
+        $docUrl = "http://docs.splunk.com$($link.href)"
+        $docPage = Invoke-WebRequest -Uri $docUrl
+        $docManualPdfUrl = ($docPage.Links | Where-Object {$_.class -eq "download"} | Where-Object {$_.outerText -match "Download manual as PDF"}).href
+        $docManualPdfUrl = "http://docs.splunk.com$($docManualPdfUrl)"
+        $docManualPdfUrl = $docManualPdfUrl -replace "&amp;","&"
+      
+        $client.DownloadFile($docManualPdfUrl,$downloadfile) 
+
     }
 }
 
